dabf6fb3c3a4a2caeea7fad1955aa13cce43676d
majorsopa <73245063+majorsopa@users.noreply.github.com>
2021-01-07 20:44:59 -0500

    Update main.py
    Made more "pythonic"

--- src/main.py
+++ src/main.py
@@ -6,10 +6,10 @@ path = os.path.join(dirname, 'files to change')
 window = tk.Tk(className = 'extention changer')
 window.geometry('800x250')
 
-text_box_frame = tk.Frame(master = window, width = 30, height = 10)
-extention_to_change_to = tk.Entry(width = 30)
+textBoxFrame = tk.Frame(master = window, width = 30, height = 10)
+extentionToChangeTo = tk.Entry(width = 30)
 
-def change_extention(ending_extention):
+def changeExtention(endingExtention):
 
   f = []
 
@@ -22,13 +22,13 @@ def change_extention(ending_extention):
     for x in filenames:
 
       file = filenames[j]
-      splitat = file.rfind('.') + 1
+      splitAt = file.rfind('.') + 1
 
-      r = file[splitat:]
-      length_to_remove = len(r)
-      file = file[:-length_to_remove]
+      r = file[splitAt:]
+      lengthToRemove = len(r)
+      file = file[:-lengthToRemove]
 
-      file += ending_extention
+      file += endingExtention
       os.rename(path + '/' + filenames[j] , path + '/' + file)
 
       j += 1
@@ -36,23 +36,23 @@ def change_extention(ending_extention):
     break
 
 def handle_button_press(event): 
-  change_extention(extention_to_change_to.get())
+  changeExtention(extentionToChangeTo.get())
 
-button_frame = tk.Frame(master = window, width = 20, height = 10)
+buttonFrame = tk.Frame(master = window, width = 20, height = 10)
 
-convert_button = tk.Button(master = button_frame, text = 'CONVERT!!!')
-convert_button.bind('<Button-1>', handle_button_press)
+convertButton = tk.Button(master = buttonFrame, text = 'CONVERT!!!')
+convertButton.bind('<Button-1>', handle_button_press)
 
-instructions_frame = tk.Frame(master = window, width = 200, height = 200)
+instructionsFrame = tk.Frame(master = window, width = 200, height = 200)
 
-instructions = tk.Label(master = instructions_frame, text = 'Place the files you want to change the extention of in the "files to change" folder.\nAll of those file extensions will be changed to the extention you choose by typing in the box, when you press the button.\nExample of what you would type in the box, "png", "jpeg", "txt".\nKindly note the lack of "."')
+instructions = tk.Label(master = instructionsFrame, text = 'Place the files you want to change the extention of in the "files to change" folder.\nAll of those file extensions will be changed to the extention you choose by typing in the box, when you press the button.\nExample of what you would type in the box, "png", "jpeg", "txt".\nKindly note the lack of "."')
 
-instructions_frame.pack(padx = 1, pady = 4)
-button_frame.pack(padx = 1, pady = 1)
-text_box_frame.pack(padx = 1, pady = 4)
+instructionsFrame.pack(padx = 1, pady = 4)
+buttonFrame.pack(padx = 1, pady = 1)
+textBoxFrame.pack(padx = 1, pady = 4)
 
 instructions.pack()
-convert_button.pack()
-extention_to_change_to.pack()
+convertButton.pack()
+extentionToChangeTo.pack()
 
 window.mainloop()
